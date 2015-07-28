4ce16f46d8e470e7e78c9e7a38c7da1ffe91efd1
David Holm <dholmster@gmail.com>
2015-07-28 22:17:15 +0200

    libav: Shared library build

---
+++ libav.rb
@@ -0,0 +1,105 @@
+class Libav < Formula
+  desc "Audio and video processing tools"
+  homepage "https://libav.org/"
+  head "https://github.com/lu-zero/libav.git", :branch => "ancillary"
+
+  bottle do
+    sha256 "3a30c36f092ed695cc58882faf6b303efa2574bc22d66306b88a92c2784405f1" => :yosemite
+    sha256 "06f578ac6a8e1e90ce763d022c516988b8afd9cbe1a73a707f22eeb88bcf330f" => :mavericks
+    sha256 "76fc99bc066de18e660f62a6401573dedcf0652d6a0ec0c5daf6dcce8cd2cfda" => :mountain_lion
+  end
+
+  option "without-faac", "Disable AAC encoder via faac"
+  option "without-lame", "Disable MP3 encoder via libmp3lame"
+  option "without-xvid", "Disable Xvid MPEG-4 video encoder via xvid"
+
+  option "with-opencore-amr", "Enable AMR-NB de/encoding and AMR-WB decoding " \
+    "via libopencore-amrnb and libopencore-amrwb"
+  option "with-openjpeg", "Enable JPEG 2000 de/encoding via OpenJPEG"
+  option "with-openssl", "Enable SSL support"
+  option "with-rtmpdump", "Enable RTMP protocol support"
+  option "with-schroedinger", "Enable Dirac video format"
+  option "with-sdl", "Enable avplay"
+  option "with-speex", "Enable Speex de/encoding via libspeex"
+  option "with-theora", "Enable Theora encoding via libtheora"
+  option "with-libvorbis", "Enable Vorbis encoding via libvorbis"
+  option "with-libvo-aacenc", "Enable VisualOn AAC encoder"
+  option "with-libvpx", "Enable VP8 de/encoding via libvpx"
+
+  depends_on "pkg-config" => :build
+  depends_on "yasm" => :build
+
+  # manpages won't be built without texi2html
+  depends_on "texi2html" => :build if MacOS.version >= :mountain_lion
+
+  depends_on "luminem/homebrew-bmdtools/x264"
+  depends_on "faac" => :recommended
+  depends_on "lame" => :recommended
+  depends_on "xvid" => :recommended
+
+  depends_on "fontconfig" => :optional
+  depends_on "freetype" => :optional
+  depends_on "fdk-aac" => :optional
+  depends_on "frei0r" => :optional
+  depends_on "gnutls" => :optional
+  depends_on "libvo-aacenc" => :optional
+  depends_on "libvorbis" => :optional
+  depends_on "libvpx" => :optional
+  depends_on "opencore-amr" => :optional
+  depends_on "openjpeg" => :optional
+  depends_on "opus" => :optional
+  depends_on "rtmpdump" => :optional
+  depends_on "schroedinger" => :optional
+  depends_on "sdl" => :optional
+  depends_on "speex" => :optional
+  depends_on "theora" => :optional
+
+  def install
+    args = [
+      "--disable-debug",
+      "--enable-shared",
+      "--disable-indev=jack",
+      "--prefix=#{prefix}",
+      "--enable-gpl",
+      "--enable-nonfree",
+      "--enable-version3",
+      "--enable-vda",
+      "--enable-libx264",
+      "--cc=#{ENV.cc}",
+      "--host-cflags=#{ENV.cflags}",
+      "--host-ldflags=#{ENV.ldflags}",
+    ]
+
+    args << "--enable-frei0r" if build.with? "frei0r"
+    args << "--enable-gnutls" if build.with? "gnutls"
+    args << "--enable-libfaac" if build.with? "faac"
+    args << "--enable-libfdk-aac" if build.with? "fdk-aac"
+    args << "--enable-libfontconfig" if build.with? "fontconfig"
+    args << "--enable-libfreetype" if build.with? "freetype"
+    args << "--enable-libmp3lame" if build.with? "lame"
+    args << "--enable-libopencore-amrnb" if build.with? "opencore-amr"
+    args << "--enable-libopencore-amrwb" if build.with? "opencore-amr"
+    args << "--enable-libopenjpeg" if build.with? "openjpeg"
+    args << "--enable-libopus" if build.with? "opus"
+    args << "--enable-librtmp" if build.with? "rtmpdump"
+    args << "--enable-libschroedinger" if build.with? "schroedinger"
+    args << "--enable-libspeex" if build.with? "speex"
+    args << "--enable-libtheora" if build.with? "theora"
+    args << "--enable-libvo-aacenc" if build.with? "libvo-aacenc"
+    args << "--enable-libvorbis" if build.with? "libvorbis"
+    args << "--enable-libvpx" if build.with? "libvpx"
+    args << "--enable-libxvid" if build.with? "xvid"
+    args << "--enable-openssl" if build.with? "openssl"
+
+    system "./configure", *args
+
+    system "make", "install"
+  end
+
+  test do
+    # Create an example mp4 file
+    system "#{bin}/avconv", "-y", "-filter_complex",
+        "testsrc=rate=1:duration=1", "#{testpath}/video.mp4"
+    assert (testpath/"video.mp4").exist?
+  end
+end
